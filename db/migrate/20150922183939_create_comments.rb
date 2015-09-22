f466d5d3719957bc4b0225e07bae2ccdb4fe7f2e
Travis Montgomery <travisbm@gmail.com>
2015-09-22 13:51:04 -0500

    created and migrated database. verified in console.

--- db/migrate/20150922183939_create_comments.rb
+++ db/migrate/20150922183939_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.string :message
-      t.post :belongs_to
+      t.belongs_to :post, index: true, foreign_key: true
 
       t.timestamps null: false
     end
